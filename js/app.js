d926cd9ac4f09e3ae94ececb26a48b0d95771d5d
Blake Collins <blakeac369@gmail.com>
2020-06-25 17:11:54 -0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+//init GitHub
+const github = new Github;
+//Init UI
+const ui = new UI;
+
 // Search input
 const searchUser = document.getElementById('searchUser');
 
@@ -6,7 +11,19 @@ const searchUser = document.getElementById('searchUser');
 searchUser.addEventListener('keyup', (e) =>{
     //get input text
     const userText = e.target.value;
+
     if(userText !== ''){
-        
+        //Make HTTP call
+        github.getUser(userText)
+        .then(data =>{
+            if(data.profile.message === 'Not Found'){
+                //Show alert that user isn't found
+            }else{
+                //Show Profile
+                ui.showProfile(data.profile);
+            }
+        })
+    }else{
+        //Clear Profile
     }
 });
--- js/github.js
+++ js/github.js
@@ -1,8 +1,17 @@
-class GitHub {
+class Github {
     constructor(){
-        this.client_id = '';
-        this.client_secert = '';
+        this.client_id = '4e94b7127e270febd585';
+        this.client_secert = '5f537edbd3072f8c3c610e8ae8aef18e5b4e5dcd';
+    }
+    async getUser(user){
+        const profileresponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secert=${this.client_secert}`);
+        
+        const profile = await profileresponse.json();
+
+        return {
+            profile
+        }
+    
     }
 
-    
 }
