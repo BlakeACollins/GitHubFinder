3880f882c374ccb20d681a46b9bc16eae6eb5132
Blake Collins <blakeac369@gmail.com>
2023-07-07 11:53:19 -0700

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+"use client";
+
+import { Button, ButtonGroup, Container } from '@chakra-ui/react'
+
+export default function Home() {
+  return (
+    <Container maxW={'lg'}>
+    <h1> GitHub Finder version 2</h1>
+      <Button colorScheme='blue'>Button</Button>
+    </Container>
+  )
+}
